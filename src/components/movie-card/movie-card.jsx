f8b30931d2f5e4ae4772557f84d0526580cb6761
danielcli-dev <98434650+danielcli-dev@users.noreply.github.com>
2023-05-08 20:13:18 -0700

    Updated movies with API

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
     <div
@@ -8,4 +10,26 @@ export const MovieCard = ({ movie, onMovieClick }) => {
       {movie.title}
     </div>
   );
+};
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    genre: PropTypes.shape({
+      Name: PropTypes.string,
+      Description: PropTypes.string,
+    }),
+    director: PropTypes.shape({
+      Name: PropTypes.string,
+      Bio: PropTypes.string,
+      Birth: PropTypes.string,
+      Death: PropTypes.string,
+    }),
+    actors: PropTypes.array,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string,
+    Image: PropTypes.string,
+    featured: PropTypes.bool,
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired,
 };
